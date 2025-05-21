0efb1d4e0fef02ab3a08b6151b31e05daf123192
faisalhrbk <faisalhrbk@outlook.com>
2025-05-21 22:20:45 +0500

    fixed the tailwind class syntax

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+
+export default function Home() {
+  return (
+<div>
+  <Button variant="elevated">bro codezz</Button>
+  <Input placeholder="enter your name"/>
+</div>
+  );
+}
